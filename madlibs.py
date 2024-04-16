a9a2a9913e8464870f318ca44c0eba0ada1fbeaa
alisa <166948482+alisay18@users.noreply.github.com>
2024-04-15 23:48:52 -0400

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+noun = input("Noun: ")
+verb = input("Verb: ")
+verb2 = input("Verb: ")
+noun2 = input("Noun: ")
+verb3 = input("Verb: ")
+noun3 = input("Noun: ")
+adjective = input("Adjective: ")
+
+madlibs = f"I am so {noun} today. I {verb} when I will {verb2} better. {noun2} makes me want to {verb3} more than a {noun3} on a {adjective} day."
+
+print(madlibs)
